f236288a8e8137c1f476f839164aefc8db1426d6
Keagen <keegenjt@gmail.com>
2024-12-01 19:12:32 -0500

    Added start of day2 (not out yet)

---
+++ day2/target/debug/day2.d
@@ -0,0 +1,1 @@
+/root/github/advent/day2/target/debug/day2: /root/github/advent/day2/src/main.rs
---
+++ day2/target/debug/deps/day2-e242397b53fc3f48.d
@@ -0,0 +1,5 @@
+/root/github/advent/day2/target/debug/deps/day2-e242397b53fc3f48: src/main.rs
+
+/root/github/advent/day2/target/debug/deps/day2-e242397b53fc3f48.d: src/main.rs
+
+src/main.rs:
