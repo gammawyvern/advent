2409096e357a8bac78806021fef117cfddbc3517
Keagen <keegenjt@gmail.com>
2024-12-01 20:27:54 -0500

    Stuff

--- day2/target/debug/day2.d
+++
@@ -1,1 +0,0 @@
-/root/github/advent/day2/target/debug/day2: /root/github/advent/day2/src/main.rs
--- day2/target/debug/deps/day2-e242397b53fc3f48.d
+++
@@ -1,5 +0,0 @@
-/root/github/advent/day2/target/debug/deps/day2-e242397b53fc3f48: src/main.rs
-
-/root/github/advent/day2/target/debug/deps/day2-e242397b53fc3f48.d: src/main.rs
-
-src/main.rs:
