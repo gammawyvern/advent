c722ebe416b8c4079aa7a2b5e61dc34916658d65
Keagen <keegenjt@gmail.com>
2024-12-04 09:51:14 -0500

    Implement loading input puzzle

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,3 +1,27 @@
+use std::io::{self, BufReader, BufRead};
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let _puzzle = match load_input("./input.txt") {
+        Ok(puzzle) => puzzle,
+        Err(err) => {
+            eprintln!("Error loading input puzzle: {}", err);
+            std::process::exit(1);
+        }
+    };
+
+    // TODO
+}
+
+fn load_input(path: &str) -> Result<Vec<Vec<char>>, io::Error> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    let puzzle: Vec<Vec<char>> = reader 
+        .lines()
+        .filter_map(|line| line.ok())
+        .map(|line| line.chars().collect())
+        .collect();
+
+    Ok(puzzle)
 }
