3984f30900692f1247139a5b5133002155fa7a80
Keagen <keegenjt@gmail.com>
2024-12-04 13:50:56 -0500

    Implemented point checking and char matching

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -2,7 +2,7 @@ use std::io::{self, BufReader, BufRead};
 use std::fs::File;
 
 fn main() {
-    let _puzzle = match load_input("./input.txt") {
+    let puzzle = match load_input("./input.txt") {
         Ok(puzzle) => puzzle,
         Err(err) => {
             eprintln!("Error loading input puzzle: {}", err);
@@ -10,7 +10,8 @@ fn main() {
         }
     };
 
-    // TODO
+    let word = "XMAS";
+    search_puzzle(&puzzle, &word);
 }
 
 fn load_input(path: &str) -> Result<Vec<Vec<char>>, io::Error> {
@@ -24,4 +25,45 @@ fn load_input(path: &str) -> Result<Vec<Vec<char>>, io::Error> {
         .collect();
 
     Ok(puzzle)
+}
+
+// TODO Assuming properly sized puzzle and word 
+fn search_puzzle(puzzle: &Vec<Vec<char>>, word: &str) {
+    let rows = puzzle.len();
+    let cols = puzzle[0].len(); 
+
+    for row in 0..rows {
+        for col in 0..cols {
+            // if puzzle[row][col] == word.chars().nth(0) {
+            //     println!("Potential word found");
+            // }
+        }
+    }
+}
+
+fn search_for_word(puzzle: &Vec<Vec<char>>, point: (usize, usize), dir: (usize, usize), word: &str) -> bool {
+    let next_char = match word.chars().nth(0) {
+        Some(chr) => chr,
+        None => return false,
+    };
+
+    if !is_point_valid(&puzzle, point) {
+        return false;
+    }
+
+    if puzzle[point.0][point.1] != next_char {
+        return false;
+    }
+
+    true
+}
+
+fn is_point_valid(puzzle: &Vec<Vec<char>>, point: (usize, usize)) -> bool {
+    let (row, col) = point;
+
+    if row >= puzzle.len() || col >= puzzle[row].len() {
+        return false;
+    }
+
+    true
 }
